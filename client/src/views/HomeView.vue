33062fe45d6236a9f32145a5135c20ef51a94f61
ZERO <zero20022003@gmail.com>
2022-02-23 21:02:06 +0100

    bgimg2

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -6,152 +6,152 @@
 
   <main class="container">
 
- <div class="bg-primary container-large d-grid gap-2">
-        <div class="row w-50 mx-auto text-center">
-            <span class="mx-auto my-auto" style="width:75vw;"> 
-                <h1 class="h1 fs-1" style="">
-                    Bienvenue sur vos espaces clients
-                </h1>
-            </span>
-        </div>
-        <div  id="bg_espace" class="bg-light row d-grid text-center w-75 gap-3 mx-auto rounded-3">
-            <h2 class="fs-2">
-                Trouvons votre espace client !
-            </h2>
-            <p class="fs-4">Vous etes ?</p>
-            <div role="button" style="background-image: url('@/assets/img-bg-courtier.jpeg' );" class="mb-5 my-auto  btn-courtier btn-outline-primary border border-primary rounded-3 w-50 mx-auto fs-4">
-                <img class="w-25 rounded-circle" src="@/assets/img-user-courtier.jpg" alt="courtier">
-                <br>
-                <b class="fs-6">
-                    COURTIER
-                </b>
+    <div class="container-large d-grid gap-2">
+            <div class="row w-50 mx-auto text-center">
+                <span class="mx-auto my-auto" style="width:75vw;"> 
+                    <h1 class="h1 fs-1" style="">
+                        Bienvenue sur vos espaces clients
+                    </h1>
+                </span>
             </div>
-            <div role="button" style="background-image: url('@/assets/img-bg-client.jpeg' );" class="mb-5 my-auto btn-client  btn-outline-primary border border-primary rounded-3 w-50 mx-auto fs-4">
-                <img class="w-25 rounded-circle" src="@/assets/img-user-client.jpg" alt="client">
+            <div  id="bg_espace" class="bg-light row d-grid text-center w-75 gap-3 mx-auto rounded-3">
+                <h2 class="fs-2">
+                    Trouvons votre espace client !
+                </h2>
+                <p class="fs-4">Vous etes ?</p>
+                <div role="button" style="background-image: url('@/assets/img-bg-courtier.jpeg' );" class="mb-5 my-auto  btn-courtier btn-outline-primary border border-primary rounded-3 w-50 mx-auto fs-4">
+                    <img class="w-25 rounded-circle" src="@/assets/img-user-courtier.jpg" alt="courtier">
                     <br>
                     <b class="fs-6">
-                        PROFESSIONNEL BTP
+                        COURTIER
                     </b>
+                </div>
+                <div role="button" style="background-image: url('@/assets/img-bg-client.jpeg' );" class="mb-5 my-auto btn-client  btn-outline-primary border border-primary rounded-3 w-50 mx-auto fs-4">
+                    <img class="w-25 rounded-circle" src="@/assets/img-user-client.jpg" alt="client">
+                        <br>
+                        <b class="fs-6">
+                            PROFESSIONNEL BTP
+                        </b>
+                </div>
             </div>
         </div>
-    </div>
+          
+        
+            <!-- Remove the container if you want to extend the Footer to full width. -->
+    <div class="container my-5">
+      <!-- Footer -->
+      <footer
+              class="text-center text-lg-start text-dark"
+              style="background-color: #ECEFF1"
+              >
+        <!-- Section: Social media -->
+            <section class="mt-5 bg-primary">
+            <!-- Grid row-->
+            <div class="row text-center d-flex justify-content-center pt-5">
+              <!-- Grid column -->
+              <div class="col-md-2">
+                <h6 class="text-uppercase font-weight-bold">
+                  <a href="#!" class="text-white">About us</a>
+                </h6>
+              </div>
+              <!-- Grid column -->
+
+              <!-- Grid column -->
+              <div class="col-md-2">
+                <h6 class="text-uppercase font-weight-bold">
+                  <a href="#!" class="text-white">REALISATION</a>
+                </h6>
+              </div>
+              <!-- Grid column -->
+
+              <!-- Grid column -->
+              <div class="col-md-2">
+                <h6 class="text-uppercase font-weight-bold">
+                  <a href="#!" class="text-white">Contact</a>
+                </h6>
+              </div>
+              <!-- Grid column -->
+            </div>
+            <!-- Grid row-->
+          </section>
       
-    
-        <!-- Remove the container if you want to extend the Footer to full width. -->
-<div class="container my-5">
-  <!-- Footer -->
-  <footer
-          class="text-center text-lg-start text-dark"
-          style="background-color: #ECEFF1"
-          >
-    <!-- Section: Social media -->
-        <section class="mt-5 bg-primary">
-        <!-- Grid row-->
-        <div class="row text-center d-flex justify-content-center pt-5">
-          <!-- Grid column -->
-          <div class="col-md-2">
-            <h6 class="text-uppercase font-weight-bold">
-              <a href="#!" class="text-white">About us</a>
-            </h6>
-          </div>
-          <!-- Grid column -->
+        <!-- Section: Links  -->
+        <section class="">
+          <div class="container text-center text-md-start mt-5">
+            <!-- Grid row -->
+            <div class="row mt-3">
+              <!-- Grid column -->
+              <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                <!-- Content -->
+                <h6 class="text-uppercase fw-bold  letter-spacing-4">BATIWEB</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint suscipit incidunt eveniet unde enim corporis nobis eaque minima et, eos, earum veniam? Maiores dolor nulla totam fuga est, architecto natus?
+                </p>
+              </div>
+              <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-2">
-            <h6 class="text-uppercase font-weight-bold">
-              <a href="#!" class="text-white">REALISATION</a>
-            </h6>
-          </div>
-          <!-- Grid column -->
 
-          <!-- Grid column -->
-          <div class="col-md-2">
-            <h6 class="text-uppercase font-weight-bold">
-              <a href="#!" class="text-white">Contact</a>
-            </h6>
+              <!-- Grid column -->
+              <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold">Useful links</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p>
+                  <a href="#!" class="text-dark">Your Account</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-dark">Become an Affiliate</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-dark">Shipping Rates</a>
+                </p>
+                <p>
+                  <a href="#!" class="text-dark">Help</a>
+                </p>
+              </div>
+              <!-- Grid column -->
+
+              <!-- Grid column -->
+              <div id="image" class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                <!-- Links -->
+                <h6 class="text-uppercase fw-bold">Contact</h6>
+                <hr
+                    class="mb-4 mt-0 d-inline-block mx-auto"
+                    style="width: 60px; background-color: #7c4dff; height: 2px"
+                    />
+                <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+                <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
+                <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+                <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+              </div>
+              <!-- Grid column -->
+            </div>
+            <!-- Grid row -->
           </div>
-          <!-- Grid column -->
+        </section>
+        <!-- Section: Links  -->
+
+        <!-- Copyright -->
+        <div
+            class="text-center p-3"
+            style="background-color: rgba(0, 0, 0, 0.2)"
+            >
+          © 2022 Copyright:
+          <a class="text-dark" href="#"><i>AllianceTech</i></a
+            >
         </div>
-        <!-- Grid row-->
-      </section>
-  
-    <!-- Section: Links  -->
-    <section class="">
-      <div class="container text-center text-md-start mt-5">
-        <!-- Grid row -->
-        <div class="row mt-3">
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-            <!-- Content -->
-            <h6 class="text-uppercase fw-bold  letter-spacing-4">BATIWEB</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint suscipit incidunt eveniet unde enim corporis nobis eaque minima et, eos, earum veniam? Maiores dolor nulla totam fuga est, architecto natus?
-            </p>
+        <!-- Copyright -->
+      </footer>
+      <!-- Footer -->
+    <!-- End of .container -->
           </div>
-          <!-- Grid column -->
-
-
-          <!-- Grid column -->
-          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Useful links</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p>
-              <a href="#!" class="text-dark">Your Account</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Become an Affiliate</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Shipping Rates</a>
-            </p>
-            <p>
-              <a href="#!" class="text-dark">Help</a>
-            </p>
-          </div>
-          <!-- Grid column -->
-
-          <!-- Grid column -->
-          <div id="image" class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-            <!-- Links -->
-            <h6 class="text-uppercase fw-bold">Contact</h6>
-            <hr
-                class="mb-4 mt-0 d-inline-block mx-auto"
-                style="width: 60px; background-color: #7c4dff; height: 2px"
-                />
-            <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-            <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-            <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-            <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-          </div>
-          <!-- Grid column -->
-        </div>
-        <!-- Grid row -->
-      </div>
-    </section>
-    <!-- Section: Links  -->
-
-    <!-- Copyright -->
-    <div
-         class="text-center p-3"
-         style="background-color: rgba(0, 0, 0, 0.2)"
-         >
-      © 2022 Copyright:
-      <a class="text-dark" href="#"><i>AllianceTech</i></a
-        >
-    </div>
-    <!-- Copyright -->
-  </footer>
-  <!-- Footer -->
-<!-- End of .container -->
-      </div>
   </main>
 </template>
 
@@ -167,10 +167,13 @@
         scroll-behavior: auto !important;
     }
 }
-#bg_espace{
+main{
   background-image:url('../assets/img-bg-courtier.jpeg');
   background-size: cover;
   background-repeat: no-repeat;
+}
+#bg_espace{
+  opacity: .7 !important;
 }
 /* ****************** */
 /* variables */
